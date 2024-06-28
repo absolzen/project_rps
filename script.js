4ab242894e810b2e6ef7f2fb70843c4bd845a44d
absolzen <robdomu@gmail.com>
2024-06-28 21:58:01 +0200

    Add first attempt to get random choice

---
+++ script.js
@@ -0,0 +1,27 @@
+// function to get computer choice, outputs rock, paper or scissors randomly
+function getComputerChoice() {
+  //  get random int 0, 1 or 2
+  let randInt = getRandomThree();
+  // assign random int to rps string
+  return convertToChoice(randInt);
+}
+
+// get one of three random numbers
+function getRandomThree() {
+  let computerChoiceInt = Math.floor(Math.random() * 3);
+  return computerChoiceInt; 
+}
+
+// convert random int to one of rps choices
+function convertToChoice(randChoice) {
+  if (randChoice == 0) {
+    return "rock";
+  } else if (randChoice == 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+
+
